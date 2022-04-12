69371133e7eb2283888b85c7f3bfe3a95af6c29d
lizhongda <944872615@qq.com>
2022-04-12 14:39:10 +0000

    banner

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,48 @@
 <template>
-	<view>这里是首页</view>
+	<view class="home-wrap u-m-b-20">
+		<!-- 空白页 -->
+		<!-- #ifdef APP-PLUS -->
+		<u-no-network @retry="init"></u-no-network>
+		<!-- #endif -->
+		
+		<view class="content-box">
+			<!-- 导航栏 -->
+			<home-head :isScorll="isScorll" borderRadius="0"
+				:navTitle="initShop.name" :list="list"></home-head>
+			<!-- 轮播 -->
+			<!-- <sh-banner :Px="0" :Py="0"
+				:borderRadius="0" :height="530" :list="list">
+			</sh-banner> -->
+			
+		</view>
+	</view>
 </template>
 
 <script>
+	import shBanner from './components/sh-banner.vue';
+	
+	import homeHead from './index/home-head.vue';
+	
+	export default {
+		components: {
+			shBanner,
+			
+			homeHead
+		},
+		data() {
+			return {
+				list:[
+					{image: "http://file.shopro.top/uploads/20210522/edb232dbdf32b7e4df0eb6d079d471b6.png"},
+					{image: "http://file.shopro.top/uploads/20210522/89e158b63896889422342043af5e741f.png"}
+				],
+				isScorll: false
+			};
+		},
+		onPageScroll(e) {
+			this.isScorll = e.scrollTop > 100 ? true : false;
+		},
+		
+	}
 </script>
 
-<style>
-</style>
+<style lang="scss"></style>
